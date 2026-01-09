7f5940c6d8e226ea989ffe4ab420dda6beec8cea
Adrian van Dixhoorn <adrian123van@gmail.com>
2026-01-09 15:55:46 +0100

    gitflow-feature-stash: FuncionesLogica

--- TDC/Assets/Adrian/Scripts/FaceCamera.cs
+++ TDC/Assets/Adrian/Scripts/FaceCamera.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class FaceCamera : MonoBehaviour
 {
     private Camera cam;
+    private float updateInterval = 0.1f; // Solo actualizar cada 100ms
+    private float nextUpdate = 0f;
 
     private void Start()
     {
@@ -11,7 +13,11 @@ public class FaceCamera : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (Time.time < nextUpdate) return;
+        
         if (cam != null)
             transform.LookAt(transform.position + cam.transform.forward);
+        
+        nextUpdate = Time.time + updateInterval;
     }
 }
